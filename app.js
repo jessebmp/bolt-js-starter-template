f4375907deae3cc7002bdc0a01d476c4023d7372
jessebmp <jesse@bmpcreative.com>
2024-03-29 13:56:05 -0500

    init

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// This version responds to @'ing the bot
 const { App, LogLevel } = require('@slack/bolt');
 const { config } = require('dotenv');
 const { registerListeners } = require('./listeners');
@@ -14,6 +15,16 @@ const app = new App({
 
 /** Register Listeners */
 registerListeners(app);
+
+// First attempt at @'ing the bot
+app.event('app_mention', async ({ event, say }) => {
+  // You can access the event object here
+  // event.text will contain the text of the message
+  // event.user will contain the user ID of the user who mentioned your app
+
+  // You can use the say function to send a message to the channel where the app was mentioned
+  await say(`Hello <@${event.user}>!`);
+});
 
 /** Start Bolt App */
 (async () => {
